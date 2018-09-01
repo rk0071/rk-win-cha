f0d0b34c7cd1a0cf78005ecd8501c14d55138909
unknown <rkmlsrivilliputhur@gmail.com>
2018-09-01 05:01:33 -0700

    'updates'

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 const publicpath = path.join(__dirname , '../public');
 console.log(publicpath);
 var app = express();
 const port = process.env.PORT || 3000;
+var server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(publicpath));
-app.listen(port,() =>
+io.on('connection',(socket) =>
+{
+  console.log("user connected");
+  socket.on('disconnect',() =>
+{
+  console.log('disconnected yup');
+})
+})
+
+
+server.listen(port,() =>
 {
   console.log(`switch to ${port}`);
 })
